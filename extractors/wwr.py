bad01875ce055c40e1f546b54796f7bffec4a137
zxc88kr <zxc88kr@naver.com>
2023-02-22 13:46:01 +0900

    RemoteOk

--- extractors/wwr.py
+++ extractors/wwr.py
@@ -2,12 +2,12 @@ from requests import get
 from bs4 import BeautifulSoup
 
 def extract_wwr_jobs(keyword):
+    results = []
     base_url = "https://weworkremotely.com/remote-jobs/search?term="
     response = get(f"{base_url}{keyword}")
     if response.status_code != 200:
         print("Can't request website")
     else:
-        results = []
         soup = BeautifulSoup(response.text, "html.parser")
         jobs = soup.find_all("section", class_="jobs")
         for job_section in jobs:
@@ -25,4 +25,4 @@ def extract_wwr_jobs(keyword):
                     "link": f"https://weworkremotely.com{link}"
                 }
                 results.append(job_data)
-        return results
+    return results
--- main.py
+++ main.py
@@ -14,3 +14,36 @@ for job in jobs:
     file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
 
 file.close()
+
+'''
+from requests import get
+from bs4 import BeautifulSoup
+
+def extract_jobs(term):
+	results = []
+	url = f"https://remoteok.com/remote-{term}-jobs"
+	request = get(url, headers={"User-Agent": "Kimchi"})
+	if request.status_code != 200:
+		print("Can't get jobs.")
+	else:
+		soup = BeautifulSoup(request.text, "html.parser")
+		jobs = soup.find_all("tr", class_="job")
+		for job in jobs:
+			title = job.find("h2", itemprop="title").string.strip()
+			company = job.find("h3", itemprop="name").string.strip()
+			location = job.find("div", class_="location").string.strip()
+			link = job["data-href"]
+			job_data = {
+				"position": title,
+				"company": company,
+				"location": location,
+				"link": f"https://remoteok.com{link}"
+			}
+			results.append(job_data)
+	return results
+	
+jobs = extract_jobs("rust")
+for job in jobs:
+	print(job)
+	print("/////\n/////")
+'''
