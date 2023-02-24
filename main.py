98ca4482a273725f9db79579e17b3e5cf5d3a58d
zxc88kr <zxc88kr@naver.com>
2023-02-24 16:43:12 +0900

    Flask

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from extractors.wwr import extract_wwr_jobs
 from extractors.indeed import extract_indeed_jobs
 from extractors.remote import extract_remote_jobs
+from file import save_to_file
+from flask import Flask
 
+app = Flask("JobScrapper")
+
+app.run("0.0.0.0")
+
+'''
 keyword = input("What do you want to search for? ")
 
 wwr = extract_wwr_jobs(keyword)
@@ -9,10 +16,5 @@ indeed = extract_indeed_jobs(keyword)
 remote = extract_remote_jobs(keyword)
 jobs = wwr + indeed + remote
 
-file = open(f"{keyword}.csv", "w", encoding="utf-8-sig")
-file.write("Position,Company,Location,URL\n")
-
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
-
-file.close()
+save_to_file(keyword, jobs)
+'''
